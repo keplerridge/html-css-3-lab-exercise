27b8b6f03c47ecfbd112a07f45e489fca4b5b6ae
keplerridge <kepler.ridge@gmail.com>
2021-01-07 16:15:06 -0700

    finished afternoon project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import {Component} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      dropdown: false
+    }
+  }
+
+  toggleDropdown = () => {
+    this.setState({dropdown: !this.state.dropdown});
+  }
+  
+  render (){
+    return (
+      <div>
+        <header>
+          <p id='bootstrap'>Start Bootstrap</p>
+          <section className='navbar'>
+            <span>SERVICES</span>
+            <span>PORTFOLIO</span>
+            <span>ABOUT</span>
+            <span>TEAM</span>
+            <span>CONTACT</span>
+          </section>
+          <div className='dropdown' onClick={this.toggleDropdown}>Menu</div>
+          {this.state.dropdown
+          ? (
+            <nav className='mobile-dropdown'>
+              <span>SERVICES</span>
+              <span>PORTFOLIO</span>
+              <span>ABOUT</span>
+              <span>TEAM</span>
+              <span>CONTACT</span>
+            </nav>
+          )
+          : null}
+        </header>
+        <section className='main'>
+          <p id='welcome'>Welcome To Our Studio</p>
+          <p id='greeting'>IT'S NICE TO MEET YOU</p>
+          <button id='tell-button'>TELL ME MORE</button>
+        </section>
+      </div>
+    )
+  }
+
 }
 
 export default App;
